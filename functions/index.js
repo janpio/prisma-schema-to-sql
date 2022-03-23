e145aea0480dcbd29ce37609ade9ab44dce865b6
Jan Piotrowski <piotrowski+github@gmail.com>
2022-03-23 23:30:48 +0000

    that easy?

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,16 @@
+import tempy from 'tempy'
+const fs = require('fs')
+import {execa} from 'execa';
+
 exports.handler = async function (event, context, callback) {
-  let body = "foo"
+  const schema = event.body
+  const schemafile = tempy.file({extension: 'prisma'})
+
+  fs.writeFileSync(schemafile, schema)
+
+  const {stdout} = await execa('npx', ['prisma', 'validate', '--schema', schemafile]);
+
+  let body = stdout
   return {
     statusCode: 200,
     body: body,
