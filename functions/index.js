cb5c2994f6263df1c620d0a663b2217b647e79a2
Jan Piotrowski <piotrowski+github@gmail.com>
2022-03-24 12:56:17 +0000

    brute force

--- functions/index.js
+++ functions/index.js
@@ -8,6 +8,13 @@ path.join(process.cwd(), './node_modules/.bin/prisma')
 path.join(__dirname, '../node_modules/.bin/prisma');
 path.join(process.cwd(), '../node_modules/.bin/prisma')
 
+
+path.join(__dirname, './node_modules/prisma/package.json');
+path.join(process.cwd(), './node_modules/prisma/package.json')
+
+path.join(__dirname, '../node_modules/prisma/package.json');
+path.join(process.cwd(), '../node_modules/prisma/package.json')
+
 exports.handler = async function (event, context, callback) {
   const schema = event.body
   const schemafile = '/tmp/schema.prisma'
