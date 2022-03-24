9a62eaf7a9ff8f0259845b157c92e3b6483df45e
Jan Piotrowski <piotrowski+github@gmail.com>
2022-03-24 13:24:10 +0000

    Annotation 2

--- functions/index.js
+++ functions/index.js
@@ -3,20 +3,20 @@ import {execa} from 'execa';
 import path from 'path'
 
 const file1 = '/node_modules/.bin/prisma'
-path.join(__dirname, '.' + file1);
-// path.join(process.cwd(), '.' + file1)
+// path.join(__dirname, '.' + file1);
+path.join(process.cwd(), '.' + file1)
 // path.join(__dirname, '..' + file1);
 // path.join(process.cwd(), '..' + file1)
 
 const file2 = '/node_modules/prisma/package.json'
-path.join(__dirname, '.' + file2);
-// path.join(process.cwd(), '.' + file2)
+// path.join(__dirname, '.' + file2);
+path.join(process.cwd(), '.' + file2)
 // path.join(__dirname, '..' + file2);
 // path.join(process.cwd(), '..' + file2)
 
 const file3 = '/node_modules/@prisma/engines/libquery_engine-rhel-openssl-1.0.x.so.node'
-path.join(__dirname, '.' + file3);
-// path.join(process.cwd(), '.' + file3)
+// path.join(__dirname, '.' + file3);
+path.join(process.cwd(), '.' + file3)
 // path.join(__dirname, '..' + file3);
 // path.join(process.cwd(), '..' + file3)
 
